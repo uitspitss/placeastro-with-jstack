d2ca59c22cf1655566fafa29dad9fa2938daa7c1
uitspitss <3781626+uitspitss@users.noreply.github.com>
2025-02-26 21:53:44 +0900

    chore: setup local development

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
 import 'dotenv/config';
 
-export default defineConfig({
+const prodConfig = {
   out: './drizzle',
   schema: './src/server/db/schema.ts',
   driver: 'd1-http',
@@ -11,4 +11,13 @@ export default defineConfig({
     databaseId: process.env.CLOUDFLARE_DATABASE_ID as string,
     token: process.env.CLOUDFLARE_D1_TOKEN as string,
   },
-});
+} satisfies Config;
+const localConfig = {
+  schema: './src/server/db/schema.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: process.env.LOCAL_DB_PATH as string,
+  },
+} satisfies Config;
+
+export default process.env.NODE_ENV === 'production' ? prodConfig : localConfig;
