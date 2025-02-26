880d1520fd835e4d42bf9c7f987b6cc7c85c46b5
uitspitss <3781626+uitspitss@users.noreply.github.com>
2025-02-27 02:21:33 +0900

    fix: add getClient

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -13,6 +13,11 @@ const getBaseUrl = () => {
  * Your type-safe API client
  * @see https://jstack.app/docs/backend/api-client
  */
-export const client = createClient<AppRouter>({
-  baseUrl: `${getBaseUrl()}/api`,
-});
+// export const client = createClient<AppRouter>({
+//   baseUrl: `${getBaseUrl()}/api`,
+// });
+
+export const getClient = () =>
+  createClient<AppRouter>({
+    baseUrl: `${getBaseUrl()}/api`,
+  });
