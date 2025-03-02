e76e7f1d09d1539b2817358cafebf8a4bf256237
uitspitss <3781626+uitspitss@users.noreply.github.com>
2025-03-03 01:32:12 +0900

    fix: simplify baseUrl logic in client configuration

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -8,8 +8,5 @@ import { createClient } from 'jstack';
 
 export const getClient = () =>
   createClient<AppRouter>({
-    baseUrl:
-      process.env.NODE_ENV === 'production'
-        ? 'https://placeastro-server.fruition-test-u7s.workers.dev/api'
-        : `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   });
