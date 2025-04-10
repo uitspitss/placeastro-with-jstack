2fe84d40dfd1b2b76bfa3bcd26ed7ba3633af4a0
uitspitss <3781626+uitspitss@users.noreply.github.com>
2025-04-10 23:21:36 +0900

    feat: update image fetching logic to set default dimensions and enhance landing page with tooltip and gallery components
    - Set default width and height for images in the GET request handler.
    - Refactor landing page to use new gallery component and implement clipboard copy functionality with tooltips.
    - Introduce new gallery component for displaying images in a carousel format.
    - Add aspect ratio and carousel UI components for better image presentation.
    - Implement tooltip functionality for copy buttons to enhance user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,75 @@
-import { cn } from '@/lib/utils';
-import Image from 'next/image';
-import { CopyToClipboard } from './components/copy-to-clipboard';
+'use client';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@radix-ui/react-tooltip';
+import { Clipboard } from 'lucide-react';
+import { useState } from 'react';
+import { Gallery } from './components/gallery';
 
-export default async function Home() {
+export default function Landing() {
+  const [copied, setCopied] = useState<number | null>(null);
+
+  const codeSnippets = [
+    'https://placeastro.u7s.dev/m/16',
+    'https://placeastro.u7s.dev/m/42?w=720',
+    'https://placeastro.u7s.dev/random?w=600&h=480',
+  ];
+  const galleryUrls = ['/M/1', '/M/2', '/M/3'];
+
+  const handleCopy = async (text: string, index: number) => {
+    await navigator.clipboard.writeText(text);
+    setCopied(index);
+    setTimeout(() => setCopied(null), 1500);
+  };
+
   return (
-    <main className="flex min-h-screen bg-gradient-to-br from-zinc-950 via-zinc-900 to-zinc-950 flex-col items-center justify-center relative isolate">
-      <div className="absolute inset-0 -z-10 opacity-50 mix-blend-soft-light bg-[url('/noise.svg')] [mask-image:radial-gradient(ellipse_at_center,black,transparent)]" />
-      <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16">
-        <div className="flex-col justify-start items-center flex">
-          <h1 className="text-[#dcd879] text-5xl font-bold font-['Nunito']">
-            PLACEASTRO
-          </h1>
-          <div className="text-white text-base font-bold font-['Nunito']">
-            Placeholder images of astrophotography
+    <div className="bg-background min-h-screen flex items-center justify-center">
+      <div className="container px-6 flex flex-col lg:flex-row items-center justify-center gap-16">
+        <div className="flex flex-col gap-8 max-w-[584px]">
+          <div className="flex flex-col gap-5 md:gap-4 items-center">
+            <h1 className="text-foreground text-5xl font-bold leading-[48px]">
+              placeastro
+            </h1>
+            <p className="text-muted-foreground text-lg leading-7">
+              Placeholder images of astrophotography
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            {codeSnippets.map((snippet, index) => (
+              <div
+                key={snippet}
+                className="flex items-center justify-between bg-muted text-foreground rounded px-2 py-1 w-full gap-8"
+              >
+                <code className="font-bold text-sm">{snippet}</code>
+                <TooltipProvider>
+                  <Tooltip open={copied === index} delayDuration={0}>
+                    <TooltipTrigger asChild>
+                      <button
+                        type="button"
+                        className="cursor-pointer"
+                        onClick={() => handleCopy(snippet, index)}
+                      >
+                        <Clipboard className="h-4 w-4" />
+                      </button>
+                    </TooltipTrigger>
+                    <TooltipContent
+                      className="bg-muted p-1 rounded border"
+                      sideOffset={3}
+                    >
+                      <p>copied!</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="flex-col justify-start items-start gap-3 flex">
-          <div className="flex-col justify-start items-start gap-1 flex">
-            <div className="text-white text-xl font-bold">🗒️ Examples</div>
-            <ul className={cn('text-white text-xs font-normal space-y-1')}>
-              <li className="bg-gray-950 px-4 py-1 rounded flex items-center justify-between">
-                <span>https://placeastro.u7s.dev/m/16</span>
-                <CopyToClipboard text="https://placeastro.u7s.dev/m/16" />
-              </li>
-              <li className="bg-gray-950 px-4 py-1 rounded flex items-center justify-between">
-                <span>https://placeastro.u7s.dev/m/42?w=720</span>
-                <CopyToClipboard text="https://placeastro.u7s.dev/m/42?w=720" />
-              </li>
-              <li className="bg-gray-950 px-4 py-1 rounded flex items-center justify-between">
-                <span>https://placeastro.u7s.dev/random?w=600&h=480</span>
-                <CopyToClipboard text="https://placeastro.u7s.dev/random?w=600&h=480" />
-              </li>
-            </ul>
-          </div>
-          <div className="flex-col justify-start items-start flex gap-1">
-            <div className="text-white text-xl font-bold">✅ Done</div>
-            <div className="w-72 h-11 text-white text-sm font-bold">
-              Messier (M1~5, M7~17, M19~20, M22, M27~28, M30~33, M42~45, M49,
-              M51, M53~M72, M74~M92, M98~M102, M104~110)
-            </div>
-          </div>
-        </div>
+        <Gallery imageUrls={galleryUrls} />
       </div>
-      <Image
-        src="/M/1?w=400&h=400"
-        alt="place image example"
-        width={400}
-        height={400}
-        placeholder="blur"
-        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkAAIAAAoAAv/lPAAAAABJRU5ErkJggg=="
-      />
-    </main>
+    </div>
   );
 }
